3ff5b040b8d51e4bb98b11fb64660fe38d51a36c
kennycallado <kennycallado@hotmail.com>
2023-06-06 17:06:47 +0200

    UPDATE migration mínima

--- src/database/migrations/2023-04-20-092916_create_slides/up.sql
+++ src/database/migrations/2023-04-20-092916_create_slides/up.sql
@@ -15,29 +15,9 @@ ALTER TABLE slides ADD CONSTRAINT slides_slide_type_check CHECK
 
 -- SELECT diesel_manage_updated_at('slides');
 INSERT INTO slides (slide_type, title, question_id) VALUES
-  ('input', 'Título 1', 1);
-
-INSERT INTO slides (slide_type, title, content) VALUES
-  ('content', 'Título 2', 'Contenido del título 2'),
-  ('content', 'Título 3', null),
-  ('content', 'Título 4', 'Contenido del título 4'),
-  ('content', 'Título 5', 'Contenido del título 5'),
-  ('content', 'Título 6', 'Contenido del título 6'),
-  ('content', 'Título 7', 'Contenido del título 7'),
-  ('content', 'Título 8', 'Contenido del título 8')
+  ('input', 'Título 1', 1)
   ;
 
-CREATE TABLE IF NOT EXISTS questions ();
-
-ALTER TABLE questions
-  ADD COLUMN IF NOT EXISTS id SERIAL PRIMARY KEY,
-  ADD COLUMN IF NOT EXISTS question_type VARCHAR(10) NOT NULL,
-  ADD COLUMN IF NOT EXISTS question VARCHAR NOT NULL;
-
-ALTER TABLE questions ADD CONSTRAINT questions_question_type_check CHECK
-  (question_type IN ('range', 'radio', 'checkbox', 'input'));
-
-CREATE SUBSCRIPTION questions_sub
-  CONNECTION 'host=questions_db port=5432 dbname=questions user=support password=support-pass'
-  PUBLICATION questions_sub WITH (create_slot = false)
+INSERT INTO slides (slide_type, title, content) VALUES
+  ('content', 'Título 2', 'Contenido del título 2')
   ;
